85cb48a9edc2ffca97e02d229cbeb0fdc0db9529
branjbar <bij.ranjbar@gmail.com>
2014-06-24 11:33:54 +0200

    ignoring the role and focusing on document type

--- guides/queries.sql
+++ guides/queries.sql
@@ -2,8 +2,6 @@
 -- Query to group the matches based on roles and docuemnt types
 SELECT 
     score,
-    role1,
-    rol2,
     type1,
     type2,
     substring(docu2.date, 7, 12) - substring(docu1.date, 7, 12) as age,
@@ -14,7 +12,8 @@ FROM
     FROM
         links_based.miss_matches
     WHERE
-        score = 2) AS T1
+        score = 2
+    group by doc1 , doc2) AS T1
         inner join
     all_documents as docu1
         inner join
