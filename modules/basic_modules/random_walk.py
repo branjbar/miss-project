ab3301bdc1eeabf403a3b1f875ce6166aefea97a
branjbar <bij.ranjbar@gmail.com>
2014-07-03 13:09:56 +0200

    old functions for random walk and entity resolution are removed and new classes now replaced them.

--- modules/basic_modules/random_walk.py
+++ modules/basic_modules/random_walk.py
@@ -1,151 +1,121 @@
 __author__ = 'Bijan'
 
+from modules.basic_modules import basic
 import networkx as nx
 import random
 import matplotlib
 matplotlib.use('TkAgg')
 import pylab
 import math
+import copy
 
 # TODO: Replace the current random walk with the Java code that Hossein gave me.
 
-CONVERSION_THRESHOLDS = 0.00001
+CONVERSION_THRESHOLDS = 0.0001
 CONVERSION_ITERATIONS = 10000
 
 
 def l2(a, b):
+    """
+    finds the l2 norm between two dictionaries
+    """
     result = 0
-    for i in xrange(len(a)):
-        result += (a[i] - b[i]) * (a[i] - b[i])
+    for key in a.keys():
+        result += (a[key] - b[key]) * (a[key] - b[key])
 
     return math.sqrt(result)
 
 
 class RandomWalk():
+    """
+    This function computes the steady state distribution of Random Walk with Restarts (RWR):
+    S^j(t+1) = (1-r)M^TS^j(t) + rX
+    where M is the adjacency matrix and M is row normalized. S^j is the neighborhood profile of node j.
+    Component S^j_i is proportional to the frequency of visits to node in the RWR from node j.
+    X is the initial state of the random walk. When S^j(t) converges, we'll get the final neighborhood profile.
+
+    Reference: Petko Bogdanov and Ambuj Singh, "Function Prediction Using Neighborhood Patterns"
+
+    """
     def __init__(self, graph):
-        """
-        initializes the RandomWalk class by a graph
+        """ (graph)
+        initializes the RandomWalk class
+        graph: a networkx graph
         """
 
         self.graph = graph   # a graph in networkx structure
         self.n = self.graph.number_of_nodes()  # size of graph
+        self.x_initial = {}
+        self.proximity_dict = {}  # stores the final results with respect to starting nodes
+        self.start_nodes = []  # stores list of starting nodes for the random walk
 
-    def run_uniform(self, restart):
-        """ (int) --> (list)
+    def generate_x_initial(self, start_nodes):
+        """
+        generates the x_initial w.r.t. the list of starting nodes
+        """
+        self.start_nodes = start_nodes
+        self.x_initial = {node: 0 for node in self.graph.nodes()}
+        for node in start_nodes:
+            self.x_initial[node] = 1.0 / len(start_nodes)
+
+
+
+    def run_uniform(self, restart=.3):
+        """ (list, int) --> (list)
+
+        x_initial: initial state of random walk. Usually all of its elements are zero except the target node(s).
+                   if we put every node 1/n, then we'll get a page rank.
 
         """
-        x_initial = [1.0/self.n] * self.n   # know as X in the paper
-        x_old = [1.0/self.n] * self.n
+
+        x_old = copy.deepcopy(self.x_initial)  # known as S(t) in the paper
         diff = 1.0
         it = 0
 
         while diff > CONVERSION_THRESHOLDS and it < CONVERSION_ITERATIONS:
-            x = [0] * self.n
-            for edge in self.graph.edges():
-                x[edge[0]] += (1.0 - restart) * (1.0 / self.graph.degree(edge[1])) * x_old[edge[1]]
-                # x[self.am[i][0]] += (1.0-restart) * self.w[i] * xold[self.am[i][1]]
+            if it and not it % 1000:
+                basic.log("RWR algorithm is in iteration %d and steady state error is %.3f" % (it, diff))
 
-            for i in xrange(self.n):
-                x[i] += restart * x_initial[i]
+            x = {node: 0 for node in self.graph.nodes()}  # initializing a zero vector
+
+            for edge in self.graph.edges():
+                w_target = 1.0 / self.graph.degree(edge[1])  # normalizing the weights
+                w_source = 1.0 / self.graph.degree(edge[0])  # normalizing the weights
+
+                x[edge[0]] += (1.0 - restart) * w_target * x_old[edge[1]]
+                x[edge[1]] += (1.0 - restart) * w_source * x_old[edge[0]]
+
+            for node in self.graph.nodes():
+                x[node] += restart * self.x_initial[node]
 
             diff = l2(x, x_old)
             it += 1
-            x_old = x[:]
+            x_old = copy.deepcopy(x)
 
-        return x_old
-
-
-# one shot random_walk
-def single_random_step(graph, node_s, visited=[]):
-
-    next_node = None
-    neighbors = graph.neighbors(node_s)
-    feasible_neighbors = list(set(neighbors) - set(visited))  # excluding already visited nodes
-    if feasible_neighbors:
-        next_node = random.choice(feasible_neighbors)
-
-    return next_node
-
-
-def random_walk(graph, node_s, restart_c=0.1):
-    """
-        starts from node_s and moves ahead until it decides to restart,
-        at this point it exports the one shot proximity dict
-    """
-    next_node = single_random_step(graph, node_s)
-    if not next_node and not next_node == 0:
-        return [node_s]  # just in case the node is isolated
-
-    visited = [node_s, next_node]
-    while random.random() >= restart_c:
-        next_node = single_random_step(graph, visited[-1], visited)
-
-        if not next_node:
-            return visited
-
-        visited.append(next_node)
-
-    return visited
-
-
-# proximity detection
-def get_proximity(graph, node_s, max_iteration, restart_c=.5):
-    """ (graph, node, float) --> dict
-        returns the proximity vector that contains the closeness of each node to node_s
-    """
-
-    proximity = {}
-    counter = 0  # counts number of visited nodes
-    if restart_c == 0:
-        restart_c = 0.05  # just to avoid getting stuck in a never ending loop
-
-    visitor_list = []
-    counter = 0
-    while counter < max_iteration :
-        visitor_list.append(random_walk(graph, node_s, restart_c))
-
-        if not len(visitor_list[-1]):
-            counter = max_iteration  # in case of having an isolated node, we'll get a null vector immediately
-
-        counter += len(visitor_list[-1])
-
-    for visit in visitor_list:
-        distance = 0  # distance to node_s
-        for node in visit[1:]:  # to skip node_s which is always the first in array
-            distance += 1
-            proximity[node] = 1.0 / distance + proximity.get(node,0)
-
-    return proximity
+        self.proximity_dict = copy.deepcopy(x_old)
 
 
 def main():
-    n = 40  # 30 nodes
+    n = 20  # 30 nodes
     m = 40  # 40 edges
     from modules.basic_modules.basic import log
     log('making the graph')
     graph = nx.gnm_random_graph(n,m)
     # log('making the adjcency matrix')
     # A = nx.adjacency_matrix(graph)
-    log('Accessing an element')
+
     rw = RandomWalk(graph)
-    color = rw.run_uniform(.4)
-    print color
-    # print graph.neighbors(graph.nodes()[1])
-
-    # print A
+    rw.generate_x_initial([1,2])
+    rw.run_uniform(.3)
 
 
-    # nx.draw(graph)
-
-    # proximity = get_proximity(graph, 0, 40, .1)
-    # print proximity
     #
+    # print basic.get_top_k_heap_search(color, 4)
+    # print basic.get_top_k_linear_search(color, 4)
+
+    color = [rw.proximity_dict[node] for node in rw.graph.nodes()]
+
     pylab.cla()
-    # color = []
-    # proximity[0] = max(proximity.values())+1
-    # for node in graph.nodes():
-    #     color.append(proximity.get(node,0))
-    #
     positions = nx.circular_layout(graph)
     nx.draw(graph, pos=positions, node_color=color)
     pylab.show()
--- modules/record_linkage/dynak/matching_random_walk.py
+++ modules/record_linkage/dynak/matching_random_walk.py
@@ -9,8 +9,8 @@ At the end the exported csv file can be imported in mysql using:
 
 """
 import matplotlib
+
 matplotlib.use('TkAgg')
-import pylab
 import networkx
 import operator
 import time
@@ -18,121 +18,113 @@ import math
 from modules.basic_modules import basic
 from modules.basic_modules.basic import log
 from modules.record_linkage.dynak.visualize import get_family_edge
-from modules.basic_modules.random_walk import get_proximity
+from modules.basic_modules.random_walk import get_proximity, RandomWalk
 import os
+import heapq  # for finding the k largest element
 
-FILE_NAME = "../../../data/matching_kdd/matches_random_walk_%d.csv"
+FILE_NAME = "../../../data/matching_random_walk/matches_random_walk_%d.csv"
+
+MAX_BLOCK_SIZE = 30  # the maximum block size which is acceptable
 
 
-def load_graph(from_file=False, limit=1000):
-    """
-        loads a graph either from file or directly from sql
-        block_dict = {block_id: [ref1_id, ref2_id, ...]}
-        document_dict = {document_id: [ref1_id, ref2_id, ...]}
-    """
-    if not from_file:
+class EntityResolution():
+    def __init__(self):
 
-        log("start gathering blocks and documents")
+        self.block_dict = {}  # block_dict = {block_id: [ref1_id, ref2_id, ...]}
+        self.document_dict = {}  # document_dict = {document_id: [ref1_id, ref2_id, ...]}
+        self.reference_dict = {}  # reference_dict = {ref_id: document_id}
 
-        block_dict = {}
-        document_dict = {}
-        reference_dict = {}
-        # let's keep it as light as possible
-        block_query = """
-                        SELECT id, block_id, register_id
-                        FROM links_based.all_persons_new
-                        LIMIT %d
-                      """ % limit
-        db = basic.do_connect()
-        cur = basic.run_query(block_query)
+        self.graph = networkx.Graph()  # a graph, nodes: references and blocks, edges: family relations and block membership
+        self.random_walk = None
 
-        for c in cur.fetchall():
-            ref_id = c[0]
-            block_id = c[1]
-            register_id = c[2]
-            reference_dict[ref_id] = register_id
-            if block_id != 1888:
-                block_dict[block_id] = block_dict.get(block_id,[]) + [ref_id]
-            document_dict[register_id] = document_dict.get(register_id, []) + [ref_id]
+    def load_dictionary(self, from_file=False, limit=1000):
+        """
+            loads the required dictionaries that contain useful information about blocks and documents
 
+
+        """
+        if not from_file:
+
+            log("start gathering blocks and documents")
+
+
+            # let's keep it as light as possible
+            block_query = """
+                            SELECT id, block_id, register_id
+                            FROM links_based.all_persons_new
+                            LIMIT %d
+                          """ % limit
+            cur = basic.run_query(block_query)
+
+            for c in cur.fetchall():
+                ref_id = c[0]
+                block_id = c[1]
+                register_id = c[2]
+                self.reference_dict[ref_id] = register_id
+                if block_id != 1888:
+                    self.block_dict[block_id] = self.block_dict.get(block_id, []) + [ref_id]
+                self.document_dict[register_id] = self.document_dict.get(register_id, []) + [ref_id]
+
+    def load_graph(self):
+        """
+            generates a graph from the dictionaries
+            """
         log("start building the graph based on blocks")
-        G = networkx.Graph()
 
         # add block edges first
-        for block_id in block_dict.keys():
-            ref_list = block_dict[block_id]
-            if len(ref_list) < 30:  # to avoid huge graphs for now, just limit it to blocks with size less than 30
+        for block_id in self.block_dict.keys():
+            ref_list = self.block_dict[block_id]
+            if len(ref_list) < MAX_BLOCK_SIZE:  # to avoid huge graphs for now
                 for ref1 in ref_list:
                     for ref2 in ref_list:
                         if ref2 > ref1:
-                            G.add_node(ref1, block_id=block_id)
-                            G.add_node(ref2, block_id=block_id)
-                            G.add_edge(ref1, ref2)
+                            self.graph.add_node(ref1, block_id=block_id)
+                            self.graph.add_node(ref2, block_id=block_id)
+                            self.graph.add_edge(ref1, ref2)
 
         log("start adding the family relations")
 
-        for node in G.nodes():
-            edge_list = get_family_edge(document_dict[reference_dict[node]])
+        for node in self.graph.nodes():
+            edge_list = get_family_edge(self.document_dict[self.reference_dict[node]])
             for e in edge_list:
-                G.add_edge(e[0], e[1])
+                self.graph.add_edge(e[0], e[1])
 
-    return G
+    def get_similars(self, reference, restart=.3):
+        """
+        """
+        similarity_list = []
+        if not self.random_walk:
+            self.random_walk = RandomWalk(self.graph)
 
+        self.random_walk.generate_x_initial([reference])
+        self.random_walk.run_uniform(restart)
+        k_keys_sorted_by_values = heapq.nlargest(10, self.random_walk.proximity_dict,
+                                                 key=self.random_walk.proximity_dict.__getitem__)
+        # here the problem is that many of the similar nodes are not acceptable:
+        #   * being equal to the reference
+        #   * being in the same document as the reference
+        #   * not being in the same block as reference
+        #   * some returned similars have a proximity of zero
 
-    # log("Visualizing the graph")
+        for key in k_keys_sorted_by_values:
+            if not key == reference \
+                    and self.graph.node[reference].get('block_id') == self.graph.node[key].get('block_id') \
+                    and (reference - key > 3 or reference - key < -3)\
+                    and self.random_walk.proximity_dict[key]>0:
+                similarity_list.append([key, self.random_walk.proximity_dict[key]])
 
-    # print networkx.communicability_exp(G)
-    # pylab.cla()
-    # positions = networkx.circular_layout(G)
-    # networkx.draw(G, pos=positions)
-    # NX.write_gml(network, "graph_for_gephi.gml")
-    # pylab.show()
-
-def get_similarity_random_walk(graph, max_iteration=100, reset_c=.5):
-    """ (graph) --> (list)
-        for a given graph, return a list of feasible matches
-    """
-    try:
-        os.remove(FILE_NAME % max_iteration)  # to be sure data will not be appended to a non-empty file
-    except:
-        pass
-
-
-    log("walking randomly")
-    now = time.time()
-    count = 1
-    csv_text = ''
-    for ref1 in graph.nodes():
-        block_id = graph.node[ref1].get('block_id')
-        if block_id:
-            proximity = get_proximity(graph, ref1, max_iteration, reset_c)
-            proximity = sorted(proximity.iteritems(), key=operator.itemgetter(1), reverse=True)  # sorting dict
-
-            for link in proximity:
-                if graph.node[link[0]].get('block_id') == block_id \
-                        and math.fabs(link[0] - ref1) > 3: # latter is to avoid father_child match
-                    ref2 = link[0]
-                    score = link[1]
-                    # print count, ref1, '--', link[0], '(', link[1], ')'
-                    count += 1
-                    csv_text += str(count) + ',' + str(ref1) + ',' + str(ref2) + ',' + str(score) +  '\n'
-                    if not count % 10000:
-                        log(count)
-                        with open(FILE_NAME % max_iteration, "a") as my_file:
-                            my_file.write(csv_text)
-                        csv_text = ''
-
-    with open(FILE_NAME % max_iteration, "a") as my_file:
-        my_file.write(csv_text)
-    csv_text = ''
-
-    log("elapsed time for %d nodes (i.e., %d matches) is %s" % (len(graph.nodes()), count, str(time.time() - now) ))
+        return similarity_list
 
 
 def main():
+    entity_resolution = EntityResolution()
+    entity_resolution.load_dictionary(False, 1000)
+    entity_resolution.load_graph()
 
-    graph = load_graph(from_file=False, limit=6000000)
-    get_similarity_random_walk(graph, max_iteration=100, reset_c=.5)
+    for node in entity_resolution.graph.nodes():
+        similars_list = entity_resolution.get_similars(node)
+        print 'random walk for %d' % node, similars_list
+
 
 if __name__ == "__main__":
     main()
