23bffb1d06217fbd1aa0d97657cded65f297ec6f
bijan_air <bij.ranjbar@gmail.com>
2014-10-02 16:27:34 +0200

    now we have 5000 nice stories!

--- modules/record_linkage/intelligent_stories.py
+++ modules/record_linkage/intelligent_stories.py
@@ -21,7 +21,7 @@ class Story():
         start = 0
         num_found = 10000000
         while start <= num_found:
-            response = self.s.query('id:3000*', rows=100, start=start, fields="blockKeys")
+            response = self.s.query('id:30*', rows=100, start=start, fields="blockKeys")
             num_found = response.numFound
             for r in response.results:
                 for b in r.get('blockKeys', []):
@@ -32,10 +32,20 @@ class Story():
 
 
     def find_interesting_stories(self):
+        count = 0
         for block_key in self.unique_blocks:
-            response = self.s.query('blockKey=' + block_key, )
-            if response.numFound > 10 and block_key.split('_')[2:4] != block_key.split('_')[8:10]:
-                print block_key, response.numFound
+            flag_n = False
+            flag_c = False
+            response = self.s.query('blockKey=' + block_key, fields="id")
+            if response.numFound > 1 and block_key.split('_')[2:4] != block_key.split('_')[8:10] and not '>' in block_key:
+                for result in response.results:
+                    if int(result['id']) < 30000000:
+                        flag_c = True
+                    else:
+                        flag_n = True
+                if flag_c and flag_n:
+                    print count, block_key, response.numFound
+                    count += 1
 
 if __name__ == '__main__':
     story = Story()
