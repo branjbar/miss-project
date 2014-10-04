93e8a84a11f6ffbbbda20fbed4a8def21a09048c
branjbar <bij.ranjbar@gmail.com>
2014-10-04 22:33:58 +0200

    2014 data, xml to database.

---
+++ modules/data_import/bs_birth.py
@@ -0,0 +1,110 @@
+from modules.basic_modules import basic
+
+__author__ = 'bijan'
+
+import untangle
+xmldoc = '/Users/Bijan/sandbox/Database/MISS-exports/BS-birth_a69dbe15-9c9b-b331-6497-f4e8c5288bf1.xml'
+xml_file = open(xmldoc, 'r')
+flag = 0
+threshold = 4
+# reading the header
+person_id = 0
+
+while not flag :
+    query = ""
+    new_line = xml_file.readline()
+    vader_terminate = """<field name="Vader" label="Vader" formtype="ChildEntity"/>"""
+    if "export" in new_line:
+        flag = 1
+
+doc_id = 1
+xml_2_sql = open('xml_to_sql_birth_2014.sql', 'a')
+while 1:
+    document = ''
+    flag = 0
+    while flag < threshold:
+        new_line = xml_file.readline()
+        document += new_line
+        vader_terminate = """<field name="Vader" label="Vader" formtype="ChildEntity"/>"""
+        moeder_terminate = """<field name="Moeder" label="Moeder" formtype="ChildEntity"/>"""
+        kind_terminate = """<field name="Kind" label="Kind" formtype="ChildEntity"/>"""
+
+        if not (not ("/record" in new_line) and not (vader_terminate in new_line) and not (
+            moeder_terminate in new_line) and not (kind_terminate in new_line)):
+            flag += 1
+    register = {'uuid': '', 'date': ''}
+    kid = {'uuid': '', 'birth_date': '', 'first_name': '', 'prefix': '', 'last_name': ''}
+    father = {'uuid': '', 'first_name': '', 'prefix': '', 'last_name': ''}
+    mother = {'uuid': '', 'first_name': '', 'prefix': '', 'last_name': ''}
+
+    try:
+        obj = untangle.parse(document)
+        try: register['uuid'] = obj.record['uuid'] # get unique id
+        except: pass
+        try: register['date'] = obj.record.field[4].children[0].cdata
+        except: pass
+        
+        try: kid['uuid'] = obj.record.field[5].entity['uuid']
+        except: pass
+        try: kid['birth_date'] =  obj.record.field[5].entity.record.field[5].children[0].cdata
+        except: pass
+
+        try: kid['first_name'] = obj.record.field[5].entity.record.field[6].children[0].cdata
+        except: pass
+        try: kid['prefix'] = obj.record.field[5].entity.record.field[7].children[0].cdata
+        except: pass
+        try: kid['last_name'] = obj.record.field[5].entity.record.field[8].children[0].cdata
+        except: pass
+
+
+        try: father['uuid'] = obj.record.field[6].entity['uuid']
+        except: pass
+        try: father['first_name'] = obj.record.field[6].entity.record.field[3].children[0].cdata
+        except: pass
+        try: father['prefix'] = obj.record.field[6].entity.record.field[5].children[0].cdata
+        except: pass
+        try: father['last_name'] = obj.record.field[6].entity.record.field[6].children[0].cdata
+        except: pass
+
+        try: mother['uuid'] = obj.record.field[7].entity['uuid']
+        except: pass
+        try: mother['first_name'] = obj.record.field[7].entity.record.field[3].children[0].cdata
+        except: pass
+        try: mother['prefix'] = obj.record.field[7].entity.record.field[5].children[0].cdata
+        except: pass
+        try: mother['last_name'] = obj.record.field[7].entity.record.field[6].children[0].cdata
+        except: pass
+
+    except:
+        print document
+        break
+
+
+    person_id += 1
+    query += """
+            INSERT INTO `all_persons_2014` (`id`, `uuid`, `first_name`, `prefix`, `last_name`, `date_1`, `role`, `register_id`, `register_type`)
+            VALUES (%d,"%s","%s","%s","%s","%s", %d, %d, "%s");
+            """ % (person_id, kid['uuid'], kid['first_name'], kid['prefix'], kid['last_name'], kid['birth_date'], 1, doc_id, 'birth')
+    person_id += 1
+    query += """
+            INSERT INTO `all_persons_2014` (id, uuid, `first_name`, prefix, `last_name`, date_1, role, `register_id`, `register_type`)
+            VALUES (%d,"%s","%s","%s","%s","%s", %d, %d, "%s");
+            """ % (person_id, father['uuid'], father['first_name'], father['prefix'], father['last_name'], kid['birth_date'], 5, doc_id, 'birth')
+    person_id += 1
+    query += """
+            INSERT INTO `all_persons_2014` (id, uuid, `first_name`, prefix, `last_name`, date_1, role, `register_id`, `register_type`)
+            VALUES (%d,"%s","%s","%s","%s","%s", %d, %d, "%s");
+            """ % (person_id, mother['uuid'], mother['first_name'], mother['prefix'], mother['last_name'], kid['birth_date'], 5, doc_id, 'birth')
+
+    query += """
+            INSERT INTO `all_documents_2014` (id, uuid, `type_text`, date, `municipality`, reference_ids)
+            VALUES (%d,"%s","%s","%s","%s","%s");
+            """ % (doc_id, register['uuid'], 'birth', register['date'], 'ERR', ','.join([str(person_id-2), str(person_id-1), str(person_id)]))
+
+    if not doc_id % 1000:
+        print doc_id
+
+        xml_2_sql.write(query.encode('utf-8'))
+        query = ""
+
+    doc_id += 1
---
+++ modules/data_import/bs_marriage.py
@@ -0,0 +1,254 @@
+"""
+Here we convert the BS-marraige file into a SQL command!
+
+
+To import the sql file use:
+>> mysql links_based -uroot< sql_bs_marriage.sql
+"""
+__author__ = 'bijan'
+
+import untangle
+
+INPUT_FILE_NAME = "/Users/Bijan/sandbox/Database/MISS-exports/BS-marriage_6899c444-4a46-b2fc-0f78-8d24de2dc491.xml"
+OUTPUT_FILE_NAME = "sql_bs_marriage.sql"
+FIRST_MARRIAGE_DOCUMENT_ID = 2000000
+FIRST_MARRIAGE_PERSON_ID = 6000000
+
+xml_file = open(INPUT_FILE_NAME, 'r')
+xml_2_sql = open(OUTPUT_FILE_NAME, 'a')
+
+flag = 0
+# reading the header
+person_id = FIRST_MARRIAGE_PERSON_ID
+
+while not flag:
+    query = ""
+    new_line = xml_file.readline()
+    vader_terminate = """<field name="Vader" label="Vader" formtype="ChildEntity"/>"""
+    if "export" in new_line:
+        flag = 1
+
+doc_id = FIRST_MARRIAGE_DOCUMENT_ID
+threshold = 8  # counts number of <records/>
+while 1:
+    document = ''
+    flag = 0
+    while flag < threshold:
+        new_line = xml_file.readline()
+        document += new_line
+        groom_terminate = """<field name="Bruidegom" label="Bruidegom" formtype="ChildEntity"/>"""
+        bride_terminate = """<field name="Bruid" label="Bruid" formtype="ChildEntity"/>"""
+        vader_groom_terminate = """<field name="Vader bruidegom" label="Vader bruidegom" formtype="ChildEntity"/>"""
+        moeder_groom_terminate = """<field name="Moeder bruidegom" label="Moeder bruidegom" formtype="ChildEntity"/>"""
+        vader_bride_terminate = """<field name="Vader bruid" label="Vader bruid" formtype="ChildEntity"/>"""
+        moeder_bride_terminate = """<field name="Moeder bruid" label="Moeder bruid" formtype="ChildEntity"/>"""
+
+        if ("/record" in new_line) or (vader_groom_terminate in new_line) or (moeder_groom_terminate in new_line) \
+                or (vader_bride_terminate in new_line) or (moeder_bride_terminate in new_line)\
+                or (groom_terminate in new_line) or (bride_terminate in new_line):
+            flag += 1
+    register = {'uuid': '', 'date': '', 'municipality': ''}
+
+    groom = {'uuid': '', 'birth_date': '', 'birth_place': '', 'first_name': '', 'prefix': '', 'last_name': ''}
+    father_groom = {'uuid': '', 'first_name': '', 'prefix': '', 'last_name': ''}
+    mother_groom = {'uuid': '', 'first_name': '', 'prefix': '', 'last_name': ''}
+
+    bride = {'uuid': '', 'birth_date': '', 'birth_place': '', 'first_name': '', 'prefix': '', 'last_name': ''}
+    mother_bride = {'uuid': '', 'first_name': '', 'prefix': '', 'last_name': ''}
+    father_bride = {'uuid': '', 'first_name': '', 'prefix': '', 'last_name': ''}
+
+    try:
+        obj = untangle.parse(document)
+        try:
+            register['uuid'] = obj.record['uuid']  # get unique id
+        except:
+            pass
+        try:
+            register['date'] = obj.record.field[5].children[0].cdata
+        except:
+            pass
+        try:
+            register['municipality'] = obj.record.field[3].children[0].children[0].children[0].children[0].cdata
+        except:
+            pass
+
+        try:
+            groom['uuid'] = obj.record.field[6].entity['uuid']
+        except:
+            pass
+        try:
+            groom['first_name'] = obj.record.field[6].entity.record.field[3].children[0].cdata
+        except:
+            pass
+        try:
+            groom['prefix'] = obj.record.field[6].entity.record.field[4].children[0].cdata
+        except:
+            pass
+        try:
+            groom['last_name'] = obj.record.field[6].entity.record.field[5].children[0].cdata
+        except:
+            pass
+        try:
+            groom['birth_date'] = obj.record.field[6].entity.record.field[7].children[0].cdata
+        except:
+            pass
+        try:
+            groom['birth_place'] = obj.record.field[6].entity.record.field[6].children[0].cdata
+        except:
+            pass
+
+
+        try:
+            father_groom['uuid'] = obj.record.field[7].entity['uuid']
+        except:
+            pass
+        try:
+            father_groom['first_name'] = obj.record.field[7].entity.record.field[3].children[0].cdata
+        except:
+            pass
+        try:
+            father_groom['prefix'] = obj.record.field[7].entity.record.field[5].children[0].cdata
+        except:
+            pass
+        try:
+            father_groom['last_name'] = obj.record.field[7].entity.record.field[6].children[0].cdata
+        except:
+            pass
+
+        try:
+            mother_groom['uuid'] = obj.record.field[8].entity['uuid']
+        except:
+            pass
+        try:
+            mother_groom['first_name'] = obj.record.field[8].entity.record.field[3].children[0].cdata
+        except:
+            pass
+        try:
+            mother_groom['prefix'] = obj.record.field[8].entity.record.field[5].children[0].cdata
+        except:
+            pass
+        try:
+            mother_groom['last_name'] = obj.record.field[8].entity.record.field[6].children[0].cdata
+        except:
+            pass
+
+
+
+        try:
+            bride['uuid'] = obj.record.field[9].entity['uuid']
+        except:
+            pass
+        try:
+            bride['first_name'] = obj.record.field[9].entity.record.field[3].children[0].cdata
+        except:
+            pass
+        try:
+            bride['prefix'] = obj.record.field[9].entity.record.field[4].children[0].cdata
+        except:
+            pass
+        try:
+            bride['last_name'] = obj.record.field[9].entity.record.field[5].children[0].cdata
+        except:
+            pass
+        try:
+            bride['birth_date'] = obj.record.field[9].entity.record.field[7].children[0].cdata
+        except:
+            pass
+        try:
+            bride['birth_place'] = obj.record.field[9].entity.record.field[6].children[0].cdata
+        except:
+            pass
+
+
+        try:
+            father_bride['uuid'] = obj.record.field[10].entity['uuid']
+        except:
+            pass
+        try:
+            father_bride['first_name'] = obj.record.field[10].entity.record.field[3].children[0].cdata
+        except:
+            pass
+        try:
+            father_bride['prefix'] = obj.record.field[10].entity.record.field[5].children[0].cdata
+        except:
+            pass
+        try:
+            father_bride['last_name'] = obj.record.field[10].entity.record.field[6].children[0].cdata
+        except:
+            pass
+
+        try:
+            mother_bride['uuid'] = obj.record.field[11].entity['uuid']
+        except:
+            pass
+        try:
+            mother_bride['first_name'] = obj.record.field[11].entity.record.field[3].children[0].cdata
+        except:
+            pass
+        try:
+            mother_bride['prefix'] = obj.record.field[11].entity.record.field[5].children[0].cdata
+        except:
+            pass
+        try:
+            mother_bride['last_name'] = obj.record.field[11].entity.record.field[6].children[0].cdata
+        except:
+            pass
+
+
+
+    except:
+        print document
+        break
+
+    person_id += 1
+    query += """
+            INSERT INTO `all_persons_2014` (`id`, `uuid`, `first_name`, `prefix`, `last_name`, `date_1`, 'place_1',
+            `date_2`, `place_2`, `role`, `register_id`, `register_type`)
+            VALUES (%d,"%s","%s","%s","%s","%s","%s", "%s", "%s", %d, %d, "%s");
+            """ % (person_id, groom['uuid'], groom['first_name'], groom['prefix'], groom['last_name'], register['date'],
+                   register['municipality'], groom['birth_date'],  groom['birth_place'], 1, doc_id, 'marriage')
+    person_id += 1
+    query += """
+            INSERT INTO `all_persons_2014` (`id`, `uuid`, `first_name`, `prefix`, `last_name`, `date_1`, 'place_1',
+            `date_2`, `place_2`, `role`, `register_id`, `register_type`)
+            VALUES (%d,"%s","%s","%s","%s","%s","%s", "%s", "%s", %d, %d, "%s");
+            """ % (person_id, bride['uuid'], bride['first_name'], bride['prefix'], bride['last_name'], register['date'],
+                   register['municipality'], bride['birth_date'],  bride['birth_place'], 1, doc_id, 'marriage')
+
+    person_id += 1
+    query += """
+            INSERT INTO `all_persons_2014` (id, uuid, `first_name`, prefix, `last_name`, date_1, role, `register_id`, `register_type`)
+            VALUES (%d,"%s","%s","%s","%s","%s", %d, %d, "%s");
+            """ % (person_id, father_groom['uuid'], father_groom['first_name'], father_groom['prefix'], father_groom['last_name'], register['date'], 2, doc_id, 'marriage')
+
+    person_id += 1
+    query += """
+            INSERT INTO `all_persons_2014` (id, uuid, `first_name`, prefix, `last_name`, date_1, role, `register_id`, `register_type`)
+            VALUES (%d,"%s","%s","%s","%s","%s", %d, %d, "%s");
+            """ % (person_id, mother_groom['uuid'], mother_groom['first_name'], mother_groom['prefix'], mother_groom['last_name'], register['date'], 2, doc_id, 'marriage')
+
+    person_id += 1
+    query += """
+            INSERT INTO `all_persons_2014` (id, uuid, `first_name`, prefix, `last_name`, date_1, role, `register_id`, `register_type`)
+            VALUES (%d,"%s","%s","%s","%s","%s", %d, %d, "%s");
+            """ % (person_id, father_bride['uuid'], father_bride['first_name'], father_bride['prefix'], father_bride['last_name'], register['date'], 3, doc_id, 'marriage')
+
+    person_id += 1
+    query += """
+            INSERT INTO `all_persons_2014` (id, uuid, `first_name`, prefix, `last_name`, date_1, role, `register_id`, `register_type`)
+            VALUES (%d,"%s","%s","%s","%s","%s", %d, %d, "%s");
+            """ % (person_id, mother_bride['uuid'], mother_bride['first_name'], mother_bride['prefix'], mother_bride['last_name'], register['date'], 3, doc_id, 'marriage')
+
+    query += """
+            INSERT INTO `all_documents_2014` (id, uuid, `type_text`, date, `municipality`, reference_ids)
+            VALUES (%d,"%s","%s","%s","%s","%s");
+            """ % (doc_id, register['uuid'], 'marriage', register['date'], register['municipality'], ','.join([str(person_id-5), str(person_id-4), str(person_id-3), str(person_id-2), str(person_id-1), str(person_id)]))
+
+
+    if not doc_id % 1000:
+        print doc_id
+
+        xml_2_sql.write(query.encode('utf-8'))
+        query = ""
+
+    doc_id += 1
